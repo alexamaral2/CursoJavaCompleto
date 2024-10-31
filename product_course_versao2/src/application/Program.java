180ccdccc9cd3dee8434617a6060d5f8eb5631fb
Alex Junior <alexjunior2018@gmail.com>
2024-10-31 10:51:45 -0300

    adicionando exemplo

---
+++ product_course_versao2/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.println("Price: ");
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		
+		product.setPrice(1200.0);
+		System.out.println("Updated name: " + product.getPrice());
+		
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed in stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		sc.close();
+	}
+
+}
